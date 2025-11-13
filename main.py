ac52ae46b6390bc00137446e8cde29e51db1e1da
jackellingwood <ellingwoodjack@gmail.com>
2025-11-13 12:14:19 -0500

    Create structure for hash table and create basic hash function

--- main.py
+++ main.py
@@ -13,10 +13,35 @@ class DataItem:
         self.quote = line[8]
 
 
+class HashTable():
+    def __init__(self, length: int):
+        self.length = length
+        self.table = [None] * length
+
+    def add(self, value: DataItem):
+        pass
+
+    def search(self, value: DataItem):
+        pass
+
+    def remove(self, value: DataItem):
+        pass
+
+    def _hash(self, data):
+        key = 0
+        if type(data) == int:
+            pass
+        elif type(data) == str:
+            for c in data:
+                key += ord(c)
+        return key % self.length
+
+
 def main():
     with open("MOCK_DATA.csv", encoding="UTF-8") as f:
         for row in reader(f):
             DataItem(row)
+    print(HashTable(15000)._hash("asd"))
 
 
 if __name__ == "__main__":
